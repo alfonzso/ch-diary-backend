d549aba9c8cffde002d040101525a0b889ec552b
Zsolt Alföldi <zsolt.alfoldi@icellmobilsoft.hu>
2022-05-11 02:16:45 +0200

    test done

--- tests/unit/routes/auth.test.ts
+++ tests/unit/routes/auth.test.ts
@@ -1,17 +1,6 @@
-// describe('User', () => {
-//   describe('RefreshToken', () => {
-//     test('/api/auth/refreshToken', async () => {
-
-//     })
-//   })
-// })
-
 import 'reflect-metadata';
-
-// import app from '../../src/app';
 import { app, appServer } from '../../../src';
 import request from 'supertest';
-import { userRepositoryInstance } from '../../../src/repositorys';
 
 type data = {
   accessToken: string,
@@ -23,24 +12,9 @@ describe('register -> refreshToken -> delete ', () => {
   let token: data
 
   beforeAll(async () => {
-    // let response = await request(app)
-    //   .post('/api/auth/register')
-    //   .send({ email: 'foo@bar.com', password: "barbar" })
-    // token = response.body.data
-    // expect(token.accessToken.length).not.toEqual(0)
-    // expect(token.refreshToken.length).not.toEqual(0)
-
-    // let refreshTokenCookie = response.headers['set-cookie'][0]
-    //   .split(',')
-    //   .map((item: any) => item.split(';')[0])
-    //   .filter((cookie: string) => cookie.includes("refresh_token="))[0]
-    // expect(refreshTokenCookie.length).not.toEqual(0)
-
   });
 
   afterAll(async () => {
-    // let res = await userRepositoryInstance.deleteUserByEmail('foo@bar.com')
-    // expect(res.email).toEqual('foo@bar.com');
     appServer.close()
   });
 
