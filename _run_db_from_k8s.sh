7442723a1d5b9f1e62c8c9edc6fd52ddf4fbbc08
Zsolt Alföldi <zsolt.alfoldi@icellmobilsoft.hu>
2023-02-05 03:47:20 +0100

    fixing cicd

--- _run_db_from_k8s.sh
+++ _run_db_from_k8s.sh
@@ -1,1 +1,2 @@
-kubectl -n ch-diary port-forward svc/ch-diary-postgresql 35432:5432
+# kubectl -n ch-diary port-forward svc/ch-diary-postgresql 35432:5432
+kubectl -n ch-diary-test port-forward svc/ch-diary-postgresql 35432:5432
